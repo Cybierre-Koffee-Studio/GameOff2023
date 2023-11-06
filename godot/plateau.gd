b6fa07d9e5855ffe4f855260a2f341718de2c366
Thomas Blusseau <thomas.blusseau@gmail.com>
2023-11-06 23:14:30 +0100

    ça bascule ou bien ?

--- godot/plateau.gd
+++ godot/plateau.gd
@@ -26,3 +26,8 @@ func on_add_tile(src):
     var tween = create_tween()
     tween.tween_property(tuile, "position:y", 0.5, 0.8).set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT)
     tween.tween_callback(src.queue_free)
+    tip((tuile.position.x+0.5)*0.8)
+
+func tip(angle):
+    var tween = create_tween()
+    tween.tween_property(self, "rotation_degrees:z", rotation_degrees.z - angle, 0.6).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT).set_delay(0.6)
