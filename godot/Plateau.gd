699d38ea41903956dedd4a5d65abb89adb6cae35
Thomas Blusseau <thomas.blusseau@gmail.com>
2023-11-06 22:22:52 +0100

    emplacement des tuiles sur le mouse over

---
+++ godot/Plateau.gd
@@ -0,0 +1,17 @@
+extends Node3D
+
+var sceneEmplacementTuile = load("res://emplacement_tuile.tscn")
+var GRID_SIZE = 12
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    for i in GRID_SIZE*GRID_SIZE:
+        var emplacementTuile: Node3D = sceneEmplacementTuile.instantiate()
+        emplacementTuile.position.x = 5
+        emplacementTuile.position.z = 2
+        emplacementTuile.position.y = 0.5
+        add_child(emplacementTuile)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+    pass
---
+++ godot/emplacement_tuile.gd
@@ -0,0 +1,33 @@
+extends Node3D
+
+@onready var sfo:StandardMaterial3D
+var COLOR_BASE = Color("0062c0")
+var COLOR_BLUR = Color(COLOR_BASE, 0)
+var COLOR_FOCUS = Color(COLOR_BASE, 0.3)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    sfo = StandardMaterial3D.new()
+    sfo.albedo_color = COLOR_BLUR
+    sfo.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA
+    $Mesh.set_surface_override_material(0,sfo)
+    pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+    pass
+
+
+func _on_area_3d_mouse_entered():
+    print("mouse_entered")
+    var tween: Tween = create_tween()
+    tween.tween_property(sfo, "albedo_color", COLOR_FOCUS, 0.1).set_ease(Tween.EASE_OUT)
+    pass # Replace with function body.
+
+
+func _on_area_3d_mouse_exited():
+    print("mouse_exited")
+    var tween: Tween = create_tween()
+    tween.tween_property(sfo, "albedo_color", COLOR_BLUR, 0.1).set_ease(Tween.EASE_OUT)
+    pass # Replace with function body.
