0657ca48d003ee5e50b02251789e303bcddab727
Thomas Blusseau <thomas.blusseau@gmail.com>
2023-11-09 22:46:41 +0100

    placement des tuiles clé et sortie + tokens horribles

--- godot/scenes/board/board.gd
+++ godot/scenes/board/board.gd
@@ -2,21 +2,42 @@ extends Node3D
 
 signal tile_placed
 
-var tileSlotScene = load("res://scenes/board/tile_slot.tscn")
+var tileSlotScene = preload("res://scenes/board/tile_slot.tscn")
+var keyTokenScene = preload("res://scenes/tokens/key_token.tscn")
+var exitTokenScene = preload("res://scenes/tokens/exit_token.tscn")
 var GRID_SIZE = 12
 var currentTileRotation = 0
 
-# Called when the node enters the scene tree for the first time.
+const tile_template_center = preload("res://scenes/tiles/tile_center.tscn")
+const tile_template_corner = preload("res://scenes/tiles/tile_corner.tscn")
+const tile_template_corridor = preload("res://scenes/tiles/tile_corridor.tscn")
+const tile_template_straight = preload("res://scenes/tiles/tile_straight.tscn")
+
+const possible_tiles : Array[PackedScene] = [tile_template_center, tile_template_corner, tile_template_corridor, tile_template_straight]
+
 func _ready():
     for i in GRID_SIZE*GRID_SIZE:
         var tileSlot: Node3D = tileSlotScene.instantiate()
-        tileSlot.position.x = i/GRID_SIZE - 5.5
-        tileSlot.position.z = i%GRID_SIZE - 5.5
+        tileSlot.position.x = i/GRID_SIZE - GRID_SIZE/2 + 0.5
+        tileSlot.position.z = i%GRID_SIZE - GRID_SIZE/2 + 0.5
         tileSlot.position.y = 0.5
         tileSlot.connect("add_tile", on_add_tile)
         add_child(tileSlot)
+    # placement de la tuile clé
+    var keyTile = possible_tiles.pick_random().instantiate()
+    keyTile.position = Vector3(randi_range(-GRID_SIZE/2, GRID_SIZE/2-1) + 0.5, 0.5, randi_range(-GRID_SIZE/2, GRID_SIZE/2-1) + 0.5)
+    add_child(keyTile)
+    var keyToken = keyTokenScene.instantiate()
+    keyToken.position = Vector3(keyTile.position.x, keyTile.position.y + 0.1, keyTile.position.z)
+    add_child(keyToken)
+    # placement de la tuile sortie
+    var exitTile = possible_tiles.pick_random().instantiate()
+    exitTile.position = Vector3(randi_range(-GRID_SIZE/2, GRID_SIZE/2-1) + 0.5, 0.5, randi_range(-GRID_SIZE/2, GRID_SIZE/2-1) + 0.5)
+    add_child(exitTile)
+    var exitToken = exitTokenScene.instantiate()
+    exitToken.position = Vector3(exitTile.position.x, exitTile.position.y + 0.1, exitTile.position.z)
+    add_child(exitToken)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
     if Input.is_action_just_pressed("rotate_tile"):
         currentTileRotation -= deg_to_rad(90.0)
