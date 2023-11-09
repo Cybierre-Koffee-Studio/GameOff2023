0148a64d14f6a1702502ee8820fb4adb3c554bac
Lucas <l.lusson@outlook.com>
2023-11-09 14:57:28 +0100

    Oubli last push

--- godot/ProtoCrawler/coffre.gd
+++ godot/ProtoCrawler/coffre.gd
@@ -6,6 +6,8 @@ func _input(event):
    # Mouse in viewport coordinates.
     if event is InputEventMouseButton:
         if event.button_index == MOUSE_BUTTON_LEFT and event.pressed and cliquable :
+            $MeshInstance3D2.mesh.material.albedo_color = Color.GREEN
+            $MeshInstance3D.mesh.material.albedo_color = Color.GREEN
             print("Coffre ouvert!")
 
 func _on_area_3d_mouse_entered():
