b3f635929dfd1fbc950ad03d7b6fee5ea002ad2c
bart-tek <barthtek@gmail.com>
2023-11-28 00:13:04 +0100

    Mouvement caméra board

--- godot/scenes/board/free_look_camera.gd
+++ godot/scenes/board/free_look_camera.gd
@@ -1,10 +1,5 @@
 class_name FreeLookCamera extends Camera3D
 
-# Modifier keys' speed multiplier
-const SHIFT_MULTIPLIER = 2.5
-const ALT_MULTIPLIER = 1.0 / SHIFT_MULTIPLIER
-
-
 @export_range(0.0, 1.0) var sensitivity: float = 0.25
 
 # Movement state
@@ -20,15 +15,14 @@ var _bas = false
 var _gauche = false
 var _droite = false
 
-func _input(event):
-
-    if Input.is_action_just_pressed("MoveCamHaut"):
+func _input(event):    
+    if event.is_action("MoveCamHaut"):
         _haut = event.pressed
-    if Input.is_action_just_pressed("MoveCamBas"):
+    if event.is_action("MoveCamBas"):
         _bas = event.pressed
-    if Input.is_action_just_pressed("MoveCamGauche"):
+    if event.is_action("MoveCamGauche"):
         _gauche = event.pressed
-    if Input.is_action_just_pressed("MoveCamDroite"):
+    if event.is_action("MoveCamDroite"):
         _droite = event.pressed
 
 # Updates mouselook and movement every frame
@@ -40,9 +34,8 @@ func _update_movement(delta):
     # Computes desired direction from key states
     _direction = Vector3(
         (_droite as float) - (_gauche as float), 
-        
-        (_bas as float) - (_haut as float),
-        0
+        (_haut as float) - (_bas as float),
+        (_bas as float) - (_haut as float)
     )
     
     # Computes the change in velocity due to desired direction and "drag"
@@ -51,7 +44,7 @@ func _update_movement(delta):
         + _velocity.normalized() * _deceleration * _vel_multiplier * delta
     
     # Compute modifiers' speed multiplier
-    var speed_multi = 1
+    var speed_multi = 2.5
     
     # Checks if we should bother translating the camera
     if _direction == Vector3.ZERO and offset.length_squared() > _velocity.length_squared():
